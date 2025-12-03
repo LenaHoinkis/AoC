8f2c508e0a312a8b9b90b4f0b5c8c31cf861fae6
Lena Hoinkis <lena.hoinkis@sap.com>
2025-12-03 20:08:29 +0100

    2025/03: reduce redudant code

--- 2025/03/solution.py
+++ 2025/03/solution.py
@@ -6,21 +6,22 @@ with open('data.txt', 'r') as file:
     def find_first_max(l, skip_last):
         return max(enumerate(l[:-1-skip_last]), key=lambda x: x[1])
     
+    def calc_jolt(batteries,size):
+        numbers = []
+        for i in range(size-1, -1, -1):
+            index, num = (find_first_max(batteries,i))
+            batteries = batteries[index+1:]
+            numbers.append(num)
+        return int("".join(numbers))
+
     #part1
-    result = 0
+    total_jolt = 0
     for line in lines:
-        max_index, first_num = find_first_max(line,1) 
-        max_index, second_num = find_first_max(line[max_index+1:],0) 
-        result+= int(first_num+second_num)
-    print (result)
+        total_jolt += calc_jolt(line,2) 
+    print (total_jolt)
 
     #part2
-    result = 0
+    total_jolt = 0
     for line in lines:
-        numbers = []
-        for i in range(11, -1, -1):
-            i, num = (find_first_max(line,i))
-            line = line[i+1:]
-            numbers.append(num)
-        result += int("".join(numbers))
-    print(result)
+        total_jolt += calc_jolt(line,12)
+    print(total_jolt)
